0394be2dd40140ba2260707fe601ca1189b2b9dc
tarazoelee <77064834+tarazoelee@users.noreply.github.com>
2021-11-26 17:32:48 -0500

    links updated
    added links to different pages

--- backend.py
+++ backend.py
@@ -1,1 +1,39 @@
+from flask import Flask, render_template, request
+
+#render template allows us to pull in html file template 
+
+backend = Flask(__name__) #create instance of flask web application 
+
+#making web pages and defining how to access the page
+@backend.route("/", methods=["GET"]) #defining path to get to the function
+def home():
+    return render_template("index.html") #referencing the html page 
+
+@backend.route("/aboutus.html", methods=["GET"])
+def aboutus():
+    return render_template("aboutus.html")
+
+
+@backend.route("/defect.html", methods=["GET"])
+def defect():
+   return render_template("defect.html")
+
+@backend.route('/', methods=['POST'])
+def predict():
+    image = load_img(image_path, target_size=(224,224))
+    image = img_to_array(image)
+    image = preprocess_input(image)
+    yhat = model.predict(image)
+    label = decode_predictions(yhat)
+    label=label[0][0]
+
+    classification ='%s (%.2f%%)' % (label[1],label[2]*100)
+
+    return render_template('index.html', prediction=classification)
+
+
+#now running the app
+if __name__ == "__main__":
+    backend.run(port =3000, debug=True)
+
 
