5674c3684dd2613545710f1b79afd9319a2884cc
Rithik <71096898+RithikKalra@users.noreply.github.com>
2022-03-04 20:44:47 -0500

    Imported Model

--- ModelTest.py
+++ ModelTest.py
@@ -1,8 +1,26 @@
 import os
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
+import tensorflow.keras as keras
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+from PIL import Image
 
-#Need Example Dataset
-model = tf.keras.models.load_model("model90acc.h5")
-model.predict()
+def convert_image_to_array(files):
+    images_as_array=[]
+    for file in files:
+        # Convert to Numpy Array
+        images_as_array.append(img_to_array(load_img(file)))
+    return images_as_array
+
+#Retrieve the model
+model = keras.models.load_model("model90acc.h5")
+model.summary()
+
+#Retrieve the test Image
+image = Image.open("/static/Testimages/steelSheetmetal.png")
+testImg = np.array(convert_image_to_array(image))
+print('Test set shape : ',testImg.shape)
+
+#Display the prediction
+prediction = model.predict(testImg)
+print(prediction)
